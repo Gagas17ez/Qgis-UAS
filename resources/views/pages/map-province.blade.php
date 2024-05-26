e98b29ec43697142f1a07e0e9f1525fa6531eea1
santisans123 <santitok21@gmail.com>
2024-05-27 00:46:09 +0700

    Update Map

--- resources/views/pages/map-province.blade.php
+++ resources/views/pages/map-province.blade.php
@@ -8,22 +8,22 @@
             <div id="map"></div>
             <div id="controls-province">
                 <input type="checkbox" id="showPolygonProvince" name="displayOption" value="polygonProvince" checked>
-                <label for="showPolygonProvince">Tampilkan Provinsi</label>
+                <label for="showPolygonProvince">Tampilkan Batas Provinsi</label>
                 <br>
                 <input type="checkbox" id="showWaterSource" name="displayOption" value="water">
-                <label for="showWaterSource">Mata Air</label>
+                <label for="showWaterSource">Sumber Mata Air</label>
                 <br>
                 <input type="checkbox" id="showReservoirs" name="displayOption" value="reservoirs">
                 <label for="showReservoirs">Bendungan</label>
                 <br>
                 <input type="checkbox" id="showDams" name="displayOption" value="dams">
-                <label for="showDams">Dam</label>
+                <label for="showDams">DAM</label>
                 <br>
                 <input type="checkbox" id="showStationWaterQuality" name="displayOption" value="waterQuality">
                 <label for="showStationWaterQuality">Stasiun Kualitas Air</label>
                 <br>
                 <input type="checkbox" id="showWaterDrinkSystem" name="displayOption" value="waterDrink">
-                <label for="showWaterDrinkSystem">Sistem Air Minum</label>
+                <label for="showWaterDrinkSystem">Sistem Air Minum (SPAM)</label>
             </div>
         </div>
     </div>
